a4fc07036bbe3f55389f5d90131088da8fa0a667
maru-256 <maru043@docomo.ne.jp>
2021-10-23 22:08:36 +0900

    プロジェクトの設定を編集

--- src/main/java/net/kunmc/lab/paperplugintemplate/PaperPluginTemplate.java
+++ src/main/java/net/kunmc/lab/paperplugintemplate/PaperPluginTemplate.java
@@ -3,15 +3,11 @@ package net.kunmc.lab.paperplugintemplate;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PaperPluginTemplate extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
