c8d1402db941a44f3f76409fa6e1efd63380f7b0
Maru32768 <maru32768@gmail.com>
2022-11-30 11:58:18 +0900

    デバッグ用のutilメソッド

--- src/main/java/net/kunmc/lab/paperplugintemplate/PaperPluginTemplate.java
+++ src/main/java/net/kunmc/lab/paperplugintemplate/PaperPluginTemplate.java
@@ -1,6 +1,9 @@
 package net.kunmc.lab.paperplugintemplate;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class PaperPluginTemplate extends JavaPlugin {
     @Override
@@ -9,5 +12,17 @@ public final class PaperPluginTemplate extends JavaPlugin {
 
     @Override
     public void onDisable() {
+    }
+
+    public static void print(Object obj) {
+        if (Objects.equals(System.getProperty("plugin.env"), "DEV")) {
+            System.out.printf("[%s] %s%n", PaperPluginTemplate.class.getSimpleName(), obj);
+        }
+    }
+
+    public static void broadcast(Object obj) {
+        if (Objects.equals(System.getProperty("plugin.env"), "DEV")) {
+            Bukkit.broadcastMessage(String.format("[%s] %s", PaperPluginTemplate.class.getSimpleName(), obj));
+        }
     }
 }
